c7d673c1a9ee8154d7e68553b7e5b023383b4e06
Xuan-Loi Vu <loivu@luxcer.com>
2013-01-18 17:10:33 +0700

    Change from requests to urllib

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import web
-import requests
-import json
+import urllib
 import model
 
 urls = (
     '/', 'Index',
     '/authorize', 'Authorize'
 )
-
-client_id = 'e8dfbc263b67491581aae2070354fa77'
-client_secret = '313d6b3e73554f7dbc6ecd4bf8d03896'
-redirect_uri = 'http://localhost:8080/authorize'
-scope = 'likes+comments+basic+relationships'
-code = ''
-grant_type='authorization_code'
 
 render = web.template.render('templates')
 
@@ -22,36 +14,20 @@ class Authorize:
     	i = web.input()
     	if(hasattr(i, 'code')):
     		code = getattr(i, 'code')
-    		params = {
-    			'client_id' : client_id,
-    			'client_secret' : client_secret,
-    			'redirect_uri' : redirect_uri,
-    			'grant_type' : grant_type,
-    			'code' : code
-    		}
-    		
-    		instagram = "https://api.instagram.com/oauth/access_token"
-	    	print instagram
-    		response = requests.post(instagram, params)
-    		response_infor = response.json()
-    		user = response_infor['user']
-    		
-    		user_id = user['id']
-    		username = user['username']
-    		access_token = str(response_infor['access_token'])
-    		
-    		create_status = model.create_new_user(user_id, access_token, username)
-    		if(create_status == True):
-    			print	"Create user success"
-    		else:
-    			print	"Create user fail"
-    		    		
-    		return render.success(username)
-    		
+    		params = {}
+    		params['client_id'] = '41036b378e53412688f2e4520eb6ed7e'
+    		params['client_secret'] = '38ff3837ec9e4670939241a3936606ed'
+    		params['redirect_uri'] = 'http://instaliker.luxcer.com:8080/authorize'
+    		params['grant_type'] = 'authorization_code'
+    		params['code'] = code
+    		params = urllib.urlencode(params)
+    		api_url = "https://api.instagram.com/oauth/access_token"
+    		stream = urllib.urlopen(api_url, params)
+    		return stream.read()
     	else:
-	    	instagram = "https://api.instagram.com/oauth/authorize/?client_id=%s&redirect_uri=%s&response_type=code&scope=%s" \
+	    	api_url = "https://api.instagram.com/oauth/authorize/?client_id=%s&redirect_uri=%s&response_type=code&scope=%s" \
     			%(client_id, redirect_uri, scope)
-	    	raise web.seeother(instagram)
+	    	raise web.seeother(api_url)
 
 class Index:
 	def GET(self):
